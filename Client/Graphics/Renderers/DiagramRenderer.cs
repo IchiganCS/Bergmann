942ff73112c60673f442f696e3cdfc1e3e28acb1
IchiganCS <mail@ichigancs.com>
2022-07-30 00:27:33 +0200

    Professional code review by a senior developer.

--- Client/Graphics/Renderers/DiagramRenderer.cs
+++ Client/Graphics/Renderers/DiagramRenderer.cs
@@ -8,8 +8,7 @@ namespace Bergmann.Client.Graphics.Renderers;
 
 /// <summary>
 /// Renders a small rolling diagram without axes on top of a box.
-/// It makes use of <see cref="BoxRenderer.ApplyTexture"/> method, so there's no need to call it.
-/// See <see cref="TextRenderer"/> for additional details of positioning this box.
+/// See <see cref="BoxRenderer"/> for additional details of positioning this box.
 /// </summary>
 public class DiagramRenderer : BoxRenderer {
     public DiagramRenderer(int tickInterval = 500, int ticksToShow = 50) {
@@ -45,14 +44,14 @@ public class DiagramRenderer : BoxRenderer {
     /// <summary>
     /// Sets all <see cref="DataPoints"/>.
     /// </summary>
-    /// <param name="dataPoints">the new data points</param>
+    /// <param name="dataPoints">The new data points.</param>
     public void SetDataPoints(IList<float> dataPoints) {
         DataPoints = dataPoints;
     }
     /// <summary>
-    /// Adds a new data point
+    /// Adds a new data point.
     /// </summary>
-    /// <param name="value">the float (always between 0 and 1)</param>
+    /// <param name="value">The float (always between 0 and 1).</param>
     public void AddDataPoint(float value) {
         DataPoints.Add(value);
     }
@@ -62,24 +61,38 @@ public class DiagramRenderer : BoxRenderer {
     /// Advances the 
     /// </summary>
     private void Tick() {
-        float widthOfTick = Dimension.Y / TicksToShow;
-        float remainingSpace = Dimension.Y;
+        float widthOfTick = Dimension.X / TicksToShow;
+        float remainingSpace = Dimension.X;
 
         Image<Rgba32> image = new((int)Dimension.X, (int)Dimension.Y, new Rgba32(0, 0, 0));
 
         //the diagram starts at the right and is rolled over with every tick
         foreach (float value in DataPoints) {
-            remainingSpace -= widthOfTick;
             if (remainingSpace < 0)
                 break;
 
-            float valueToDraw = value < 0 ? 0 : (value > 1 ? 1 : value);
+            float valueToDraw = value < 0 ? 0 : (value > 1 ? 1 : value); //Better is Math.Clamp
             image.Mutate(x => x.FillPolygon(Color.White,
                 new PointF(0, Dimension.Y - remainingSpace),
                 new PointF(valueToDraw * Dimension.X, Dimension.Y - remainingSpace),
                 new PointF(0, Dimension.Y - remainingSpace - widthOfTick),
                 new PointF(valueToDraw * Dimension.X, Dimension.Y - remainingSpace - widthOfTick)
                 ));
+
+            // A working solution seems to be:
+            // Only OpenGL handles (0, 0) as bottom left, for most of image processing, (0, 0) refers to top(!) left.
+
+            // image.Mutate(x => x.Fill(Color.White,
+            //     new RectangleF(
+            //         new PointF(Dimension.X - remainingSpace, (1 - valueToDraw) * Dimension.Y),
+            //         new SizeF(widthOfTick, valueToDraw * Dimension.Y))
+            // ));
+
+            // it also seems nice to have a little bit of space between each bar.
+            // stated above: The diagram starts at the right. Currently, it starts left.
+            // ALTERNATIVELY: this fps display on the bottom right seems very interesting: https://i.ytimg.com/vi/Zza-iOlSlfQ/maxresdefault.jpg
+
+            remainingSpace -= widthOfTick;
         }
 
         tex.Write(image);
@@ -87,11 +100,10 @@ public class DiagramRenderer : BoxRenderer {
 
 
     /// <summary>
-    /// Renders the underlying box renderer with the specfic text on it. Binds the letter stack to texture unit
+    /// Binds the freshly made texture and renders the underlying box.
     /// </summary>
     public override void Render() {
         tex.Bind();
-        GlLogger.WriteGLError();
         base.Render();
     }
 }
